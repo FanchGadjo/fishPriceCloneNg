193e05d8ca2ff4ad5fdab76d85db0dcf32c650a6
François Kerisit <fkerisit@w2next.fr>
2020-09-07 01:55:28 +0200

    refactor: remove lorem ipsum

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-import { LoremIpsumComponent } from './lorem-ipsum/lorem-ipsum.component';
 import { MarketComponent } from './market/market.component';
 import { MarketsComponent } from './markets/markets.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, LoremIpsumComponent, MarketComponent, MarketsComponent],
+  declarations: [AppComponent, MenuComponent, MarketComponent, MarketsComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
