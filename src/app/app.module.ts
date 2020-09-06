7af8f355cbb2d3dfe031bbcebcc57cad14fc2ef7
François Kerisit <fkerisit@w2next.fr>
2020-09-07 01:37:31 +0200

    feat(market): create market component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { LoremIpsumComponent } from './lorem-ipsum/lorem-ipsum.component';
+import { MarketComponent } from './market/market.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, LoremIpsumComponent],
+  declarations: [AppComponent, MenuComponent, LoremIpsumComponent, MarketComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
